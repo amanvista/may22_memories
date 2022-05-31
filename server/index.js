20ef0c6b14b54047f5e49f2e3fbf4f9cbac6771a
Aman Bisht <amanvista@gmail.com>
2022-06-01 03:59:56 +0530

    1.Db connected

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log("hi what is happening")
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js'
+
+const app = express();
+app.use("/posts", postRoutes)
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+
+let CONNECTION_URL = "mongodb://localhost:27017/Ecommerce"
+let PORT = 5001
+//const mongoose = require('mongoose');
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.connect('mongodb://localhost:27017/Ecommerce', (err)=>{
+//       if(!err) console.log("DB Connected")
+//       else
+//       console.log("DB Error")
+//   });
